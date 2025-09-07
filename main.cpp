78b35561c7d049f9da12ce8f87f7e7b81e8e54a6
craft2guardian <bladewundke@outlook.com>
2025-09-07 04:10:04 -0700

    Made the commands in main.cpp into a map for easier editing

--- main.cpp
+++ main.cpp
@@ -9,21 +9,41 @@
 #include <filesystem>
 #include <direct.h>
 #include "commands.h"
+#include <map>
+#include <functional>
 
 int main() {
+
+
+    std::map<std::string, std::function<void(std::string)>> commands;
+
+    commands["help"] = [](std::string args){ showHelp(); };
+    commands["whoami"] = [](std::string args){ whoami(); };
+    commands["ls"] = [](std::string args){ ls(args); };
+    commands["exit"] = [](std::string args){ exit(0); };
+
+
+    std::cout << "Welcome to Wsh 0.1!\nThis project is still an early prototype so report any bugs on my Github\nhttps://github.com/craft2guardian/Wsh\nEnjoy!\n";
     std::string input;
 
     while (true) {
 
-        std::cout << getUsername() << "@" << getOEMInfo() << ":";
-
+        std::cout << getUsername() << "@" << getSanitizedHostname() << ":";
         std::getline(std::cin, input);
-
-        if (input == "help") showHelp();
-        else if (input == "whoami") whoami();
-        else if (input.substr(0, 2) == "ls") ls(input.length() > 3 ? input.substr(3) : "");
-        else if (input == "exit") break;
-        else std::cout << "wsh: " << input << ": " << "command not found\n";
-
+        if (input.empty()) {
+            continue;
+        }
+        
+        std::istringstream iss(input);
+        std::string cmd;
+        iss >> cmd;
+        std::string args;
+        std::getline(iss, args);
+        if (!args.empty() && args[0] == ' ') args = args.substr(1);
+        if (commands.find(cmd) != commands.end()) {
+            commands[cmd](args);
+        } else {
+            std::cout << "wsh: " << cmd << ": command not found\n";
+        }   
     }
 }
