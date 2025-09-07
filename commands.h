d30a81a96d1013e4fac85905998019ccd2d72bc6
craft2guardian <bladewundke@outlook.com>
2025-09-07 04:09:21 -0700

    Update commands.h

--- commands.h
+++ commands.h
@@ -6,11 +6,13 @@
 void showHelp();
 void whoami();
 void uname();
-void ls(std::string dir_path);
+void ls(const std::string& dir_path = "");
 void cd(std::string dir_path);
 void exitShell();
 
+
+
 std::string getUsername();
-std::string getOEMInfo();
+std::string getSanitizedHostname();
 
 #endif
