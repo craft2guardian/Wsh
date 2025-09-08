8410bfda7095fd5800a7cc886925b465b8bb306c
craft2guardian <bladewundke@outlook.com>
2025-09-07 22:18:54 -0700

    Update commands.h

--- commands.h
+++ commands.h
@@ -1,15 +1,24 @@
+//commands.h
+
+extern std::string targetDir;
+
+
 #ifndef COMMANDS_H
 #define COMMANDS_H
-
 #include <string>
 
+const std::string wshversion = "0.2";
+
+void pwd();
+void version();
 void showHelp();
 void whoami();
 void uname();
 void ls(const std::string& dir_path = "");
-void cd(std::string dir_path);
 void exitShell();
-
+void cd(const std::string& path);
+void run(const std::string& args);
+void clear();
 
 
 std::string getUsername();
